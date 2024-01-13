eef70f9b66a24f353048f9fe5a544d173a8fa4de
Cameron Estridge <ccestridge8@gmail.com>
2024-01-12 23:22:15 -0500

    main page and navbar changes
    various main page and navbar tweaks.

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// variables
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar-menu');
+
+const name = document.getElementById('input-name');
+const email = document.getElementById('input-email');
+const message = document.getElementById('input-message');
+const submit = document.getElementsByClassName('form-contact')[0];
+
+const navLogo = document.querySelector('#navbar-logo');
+
+// hamburger icon
 document.addEventListener('DOMContentLoaded', function () {
     const menuToggle = document.getElementById('mobile-menu');
     const navMenu = document.querySelector('.navbar-menu');
@@ -6,3 +18,9 @@ document.addEventListener('DOMContentLoaded', function () {
       navMenu.classList.toggle('active');
     });
   });
+
+  // hamburger menu
+const mobileMenu = () => {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+};
